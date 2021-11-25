6b27948c42d8ee0a9d0941c1ebf2454ee6507803
zhangsong <zhangsong@123.com>
2021-11-25 20:53:03 +0800

    二叉树

---
+++ src/com/zs/tixi/class12/T.java
@@ -0,0 +1,11 @@
+package com.zs.tixi.class12;
+
+/**
+ * 1. 实现二叉树的按层遍历。
+ *      1）其实就是宽度优先遍历，使用队列。
+ * 2. Leetcode 431.Encode N-ary Tree to Binary Tree
+ *      请将一颗多叉树用二叉树表示，并可以还原回多叉树。
+ * 3. 如何设计一个打印整颗树的打印函数。
+ */
+public class T {
+}
