fca276111f24c7a8f17f1492cbe9ac5615896d3c
zhangsong <zhangsong@123.com>
2021-12-29 20:30:28 +0800

    动态规划练习

--- src/com/zs/tixi/class19/Code02_CardsInLine.java
+++ src/com/zs/tixi/class19/Code02_CardsInLine.java
@@ -10,4 +10,39 @@ package com.zs.tixi.class19;
  *      请返回最后获胜者的分数。
  */
 public class Code02_CardsInLine {
+    /**
+     *  返回先手分数和后手分数中较大值。
+     */
+    public static int win1(int[] arr) {
+        if(arr==null || arr.length==0) return 0;
+        int p1 = f1(arr, 0, arr.length-1);
+        int p2 = g1(arr, 0, arr.length-1);
+       return Math.max(p1, p2);
+    }
+
+    /**
+     * 在L到R范围上，先手选择的分数
+     */
+    public static int f1(int[] arr, int L, int R){
+        if (L==R) return arr[L];
+        int p1 = arr[L]+g1(arr, L+1, R);
+        int p2 = arr[R]+g1(arr, L, R-1);
+        return Math.max(p1, p2);
+    }
+    /**
+     * 在L到R范围上，后手选择的分数
+     */
+    public static int g1(int[] arr, int L, int R){
+        if (L==R) return 0;
+        int p1 = f1(arr, L+1, R);
+        int p2 = f1(arr, L, R-1);
+        return Math.min(p1, p2);
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 7, 4, 5, 8, 1, 6, 0, 3, 4, 6, 1, 7 };
+        System.out.println(win1(arr));
+
+    }
 }
