595e1549a1a7a6be2deaadfce3ec9066f4b2bb46
zs <zs.123.com>
2022-02-03 23:03:31 +0800

    morris遍历

--- src/com/zs/tixi/class31/Code01_MorrisTraversal.java
+++ src/com/zs/tixi/class31/Code01_MorrisTraversal.java
@@ -31,4 +31,430 @@ public class Code01_MorrisTraversal {
             this.value = data;
         }
     }
+    public static void morris(Node head){
+        if(head == null) return;
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null){
+            System.out.print(cur.value+" ");
+            if(cur.left != null){
+                mostRight = cur.left;
+                while (mostRight.right!=null && mostRight.right!=cur){
+                    mostRight = mostRight.right;
+                }
+                if(mostRight.right == null){
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                }
+            }
+            cur = cur.right;
+        }
+        System.out.println();
+    }
+
+    /**
+     * 定义变量cur表示当前来到的节点,初始化为head.
+     * 定义变量mostRight表示左子树的最右节点,初始化为null
+     * 循环:cur不为空
+     *      如果:cur.left不为空
+     *          mostRight设置为cur.left
+     *          循环:mostRight.right不为空 且 mostRight.right不等于cur
+     *              mostRight设置为mostRight.right
+     *          如果mostRight.right等于空
+     *              mostRight.right设置为cur
+     *              cur设置为cur.left
+     *              进入下次循环..
+     *          否则
+     *              mostRight.right设置为空
+     *      cur设置为cur.right
+     */
+    public static void morris2(Node head){
+        Node cur = head;
+        Node mr = null;
+        while (cur != null){
+            System.out.print(cur.value+" ");
+            if(cur.left != null){
+                mr = cur.left;
+                while (mr.right != null && mr.right != cur){
+                    mr = mr.right;
+                }
+                if(mr.right == null){
+                    mr.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mr.right = null;
+                }
+            }
+            cur = cur.right;
+        }
+        System.out.println();
+    }
+
+    /**
+     * morris实现先序遍历.
+     * 经过两次的节点在第一次时打印.
+     */
+//    public static void morrisPre(Node head){
+//        if(head == null) return;
+//        Node cur = head;
+//        Node mostRight = null;
+//        while (cur != null){
+//            mostRight = cur.left;
+//            if(mostRight != null){
+//                while (mostRight.right!=null && mostRight.right!=cur){
+//                    mostRight = mostRight.right;
+//                }
+//                if(mostRight.right == null){
+//                    System.out.print(cur.value+" ");
+//                    mostRight.right = cur;
+//                    cur = cur.left;
+//                    continue;
+//                } else {
+//                    mostRight.right = null;
+//                }
+//            } else {
+//                System.out.print(cur.value+" ");
+//            }
+//            cur = cur.right;
+//        }
+//        System.out.println();
+//    }
+
+    /**
+     * morris实现先序:
+     * 在第一次遇到节点时打印,
+     * 有两种情况:
+     *      1. 有左树,mr.right为空
+     *      2. 无左树
+     */
+    public static void morrisPre(Node head){
+        Node cur = head;
+        Node mr = null;
+        while (cur != null){
+            if(cur.left != null){
+                mr = cur.left;
+                while (mr.right != null && mr.right != cur){
+                    mr = mr.right;
+                }
+                if (mr.right == null){
+                    System.out.print(cur.value+" ");
+                    mr.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mr.right = null;
+                }
+            } else {
+                System.out.print(cur.value+" ");
+            }
+            cur = cur.right;
+        }
+        System.out.println();
+    }
+    public static void pre(Node head){
+        processPre(head);
+        System.out.println();
+    }
+    private static void processPre(Node head){
+        if(head==null){
+            return;
+        }
+        System.out.print(head.value+" ");
+        processPre(head.left);
+        processPre(head.right);
+    }
+
+    /**
+     * morris实现中序打印.
+     * 经过两次的节点在第二次开始打印.
+     */
+    public static void morrisIn2(Node head){
+        if(head == null) return;
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null){
+            if(cur.left != null){
+                mostRight = cur.left;
+                while (mostRight.right!=null && mostRight.right!=cur){
+                    mostRight = mostRight.right;
+                }
+                if(mostRight.right == null){
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                }
+            }
+            System.out.print(cur.value+" ");
+            cur = cur.right;
+        }
+        System.out.println();
+    }
+
+    /**
+     * morris实现中序:
+     * 在第二次来到节点时打印
+     * 有两种情况:
+     *      1. 有左树,且mr.right等于cur
+     *      2. 无左树
+     */
+    public static void morrisIn(Node head){
+        Node cur = head;
+        Node mr = null;
+        while (cur != null){
+            if(cur.left != null){
+                mr = cur.left;
+                while (mr.right != null && mr.right != cur){
+                    mr = mr.right;
+                }
+                if (mr.right == null){
+                    mr.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mr.right = null;
+                }
+            }
+            System.out.print(cur.value+" ");
+            cur = cur.right;
+        }
+        System.out.println();
+    }
+    public static void in(Node head){
+        processIn(head);
+        System.out.println();
+    }
+    private static void processIn(Node head){
+        if (head==null) return;
+        processIn(head.left);
+        System.out.print(head.value+" ");
+        processIn(head.right);
+    }
+
+    /**
+     * morris实现后序打印.
+     * 第二次来到节点时,逆序打印当前节点左子树右边界.
+     * 结束后打印整棵树的右边界.
+     */
+    public static void morrisPost2(Node head){
+        if(head == null) return;
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null){
+            if(cur.left != null){
+                mostRight = cur.left;
+                while (mostRight.right!=null && mostRight.right!=cur){
+                    mostRight = mostRight.right;
+                }
+                if(mostRight.right == null){
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                    reversePrintRight(cur.left);
+                }
+            }
+            cur = cur.right;
+        }
+        reversePrintRight(head);
+        System.out.println();
+    }
+
+    /**
+     * morris实现后序遍历:
+     * 第二次来到节点,逆序打印节点左树右边界
+     *      只有一种情况:
+     *          1. 有左树,且mr.right等于cur
+     * 逆序打印整颗树的右边界.
+     */
+    public static void morrisPost(Node head){
+        Node cur = head;
+        Node mr = null;
+        while (cur != null){
+            if (cur.left != null){
+                mr = cur.left;
+                while (mr.right != null && mr.right != cur){
+                    mr = mr.right;
+                }
+                if(mr.right == null){
+                    mr.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mr.right = null;
+                    reversePrintRight(cur.left);
+                }
+            }
+            cur = cur.right;
+        }
+        reversePrintRight(head);
+        System.out.println();
+    }
+
+    public static void post(Node head){
+        processPost(head);
+        System.out.println();
+    }
+    private static void processPost(Node head){
+        if(head == null) return;
+        processPost(head.left);
+        processPost(head.right);
+        System.out.print(head.value+" ");
+    }
+
+    /**
+     * morris遍历是到达当前节点后先遍历左子树,借助左子树的最右节点的右指针回到当前节点后,再遍历右子树.
+     * 所以遍历顺序是:头,左,头,右.
+     * 而递归序的遍历顺序是:头,左,头,右,头.
+     * 递归序可以很自然的改写出先序,中序,后序.而morris改后序比较麻烦.
+     * 能不能既保留morris遍历的有点(空间复杂度O(1)),又可以实现:头,左,头,右,头的遍历顺序.
+     *
+     * 思路:
+     *      来到当前节点.
+     *      找到当前节点左树最右节点mostRight和右树最左节点mostLeft.
+     *      如果mostRight为空 且 mostLeft为空,说明无子树:
+     *          返回;
+     *      如果mostRight.right和mostLeft.left都指向null或者自身为空.说明第一次来到当前节点:
+     *          如果mostRight不为空:
+     *              将mostRight.right指向当前节点.当前节点来到左子树位置.
+     *          否则:
+     *              当前节点来到右子树.
+     *      如果mostRight.right指向当前节点,mostLeft.left指向null或自身为空.说明第二次来到当前节点:
+     *          将mostRight.right指向null,
+     *          如果mostLeft不为空:
+     *              mostLeft.left指向当前节点,
+     *          当前节点来到右子树.
+     *      如果mostRight.right指向null或自身为空,mostLeft.left指向当前节点,说明第三次来到当前节点:
+     *          如果most
+     *
+     * 思路错误!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+     *      同时使用左子树的最节点右指针和右子树最左节点左指针,会引起混乱.
+     *
+     */
+    public static void zsMorris(Node head){
+        if(head == null) return;
+
+    }
+
+    /**
+     * 反转二叉树右边界链表后逆序打印.
+     */
+    private static void reversePrintRight(Node head) {
+        Node pre = null;
+        Node cur = head;
+        Node next = head;
+        while (cur != null){
+            next = cur.right;
+            cur.right = pre;
+            pre = cur;
+            cur = next;
+        }
+        cur = pre;
+        pre = null;
+        next = cur;
+        while (cur != null){
+            System.out.print(cur.value+" ");
+            next = cur.right;
+            cur.right = pre;
+            pre = cur;
+            cur = next;
+        }
+    }
+
+    // for test -- print tree
+    public static void printTree(Node head) {
+        System.out.println("Binary Tree:");
+        printInOrder(head, 0, "H", 17);
+        System.out.println();
+    }
+
+    public static void printInOrder(Node head, int height, String to, int len) {
+        if (head == null) {
+            return;
+        }
+        printInOrder(head.right, height + 1, "v", len);
+        String val = to + head.value + to;
+        int lenM = val.length();
+        int lenL = (len - lenM) / 2;
+        int lenR = len - lenM - lenL;
+        val = getSpace(lenL) + val + getSpace(lenR);
+        System.out.println(getSpace(height * len) + val);
+        printInOrder(head.left, height + 1, "^", len);
+    }
+
+    public static String getSpace(int num) {
+        String space = " ";
+        StringBuffer buf = new StringBuffer("");
+        for (int i = 0; i < num; i++) {
+            buf.append(space);
+        }
+        return buf.toString();
+    }
+
+    public static boolean isBST(Node head) {
+        if (head == null) {
+            return true;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        Integer pre = null;
+        boolean ans = true;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                }
+            }
+            if (pre != null && pre >= cur.value) {
+                ans = false;
+            }
+            pre = cur.value;
+            cur = cur.right;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(4);
+        head.left = new Node(2);
+        head.right = new Node(6);
+        head.left.left = new Node(1);
+        head.left.right = new Node(3);
+        head.right.left = new Node(5);
+        head.right.right = new Node(7);
+//        printTree(head);
+        System.out.println("先序: ");
+        pre(head);
+        System.out.println("morris先序: ");
+        morrisPre(head);
+
+        System.out.println("中序: ");
+        in(head);
+        System.out.println("morris中序: ");
+        morrisIn(head);
+
+        System.out.println("后序: ");
+        post(head);
+        System.out.println("morris后序: ");
+        morrisPost(head);
+        printTree(head);
+
+        morris(head);
+        morris2(head);
+
+    }
 }
--- src/com/zs/tixi/class31/Code05_MinHeight.java
+++ src/com/zs/tixi/class31/Code05_MinHeight.java
@@ -4,9 +4,144 @@ package com.zs.tixi.class31;
  * 题目:
  *      给定一颗二叉树的头节点head
  *      求以head为头的树中,最小深度是多少?
+ *      最小深度:从根节点开始到最近的叶子节点经过的节点数.(注意:对于有一个子树为空的节点,最小深度并不为1,因为自身并非叶子节点)
  *
  * 二叉树递归套路可以解决的题目,如果不是强制依赖左右树的信息整合,可以考虑morris遍历.
  *
  */
 public class Code05_MinHeight {
+    private static class Node{
+        public Node left;
+        public Node right;
+        public int value;
+        public Node(int val){
+            this.value = val;
+        }
+    }
+
+    /**
+     * 思路:使用二叉树递归套路.
+     *      来到某一节点.左子树高度为h1,右子树高度为h2
+     *      当前节点高度为左右子树的最小高度+1
+     *      为什么不对 ????
+     */
+    public static int minHeight1(Node head){
+        if(head==null) return 0;
+        return p(head);
+    }
+    private static int p(Node node){
+        if (node.left == null && node.right == null) return 1;
+        int lH = Integer.MAX_VALUE;
+        if(node.left != null) lH = p(node.left);
+        int rH = Integer.MAX_VALUE;
+        if(node.right != null ) rH = p(node.right);
+        return Math.min(lH, rH)+1;
+    }
+
+    /**
+     * 使用morris遍历:
+     * 每到一个节点,想办法得到当前节点的层数.
+     *      判断当前节点是否为叶节点,如果是,与之前的答案相比,看能否比之前的答案小,如果小就更新答案.
+     * 判断当前节点的层数有两种情况:
+     *      0. 初始层数为1
+     *      1. 当前节点是上一个节点的子节点,是上一个节点的左节点或右节点,
+     *          判断依据:此时mr.right肯定不等于cur,
+     *          结论:层数等于之前层数加1.
+     *      2. 不是上一个节点的子节点,当前节点是从mr.right过来的,
+     *          判断依据:此时mr.right等于cur,
+     *          结论:层数更新为之前层数减去cur左树右边界的高度.
+     * 最终层数就是答案.
+     */
+    public static int minHeight2(Node head){
+        if (head == null) return 0;
+        int lv = 0;
+        Node cur = head;
+        Node mr = null;
+        int ans = Integer.MAX_VALUE;
+        while (cur != null){
+            if(cur.left != null){
+                mr = cur.left;
+                int mrNum = 1;
+                while (mr.right != null && mr.right != cur){
+                    mr = mr.right;
+                    mrNum++;
+                }
+                if (mr.right == null){
+                    mr.right = cur;
+                    cur = cur.left;
+                    // 当前节点为上一个节点的子节点.
+                    lv += 1;
+                    continue;
+                } else {
+                    if (mr.left == null) {
+                        ans = Math.min(ans, lv);
+                    }
+                    mr.right = null;
+                    // 当前节点是从mr.right滑过来的.
+                    lv -= mrNum;
+                }
+            } else {
+                // 当前节点为上一个节点的子节点.
+                lv += 1;
+            }
+            cur = cur.right;
+        }
+        int mrNum = 1;
+        cur = head;
+        while (cur.right != null){
+            mrNum++;
+            cur = cur.right;
+        }
+        if(cur.left == null){
+            ans = Math.min(ans, mrNum);
+        }
+
+        return ans;
+    }
+
+
+    // for test
+    public static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+
+    // for test
+    public static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (Math.random() * maxValue));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int treeLevel = 7;
+        int nodeMaxValue = 5;
+        int testTimes = 100000;
+        System.out.println("test begin");
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(treeLevel, nodeMaxValue);
+            int ans1 = minHeight1(head);
+            int ans2 = minHeight2(head);
+            if (ans1 != ans2) {
+                System.out.println(ans1);
+                System.out.println(ans2);
+                throw new RuntimeException("Oops");
+            }
+        }
+        System.out.println("test finish!");
+
+//        Node head = new Node(1);
+//        head.left = new Node(2);
+//        int ans1 = minHeight1(head);
+//        int ans2 = minHeight2(head);
+//        if (ans1 != ans2) {
+//            System.out.println(ans1);
+//            System.out.println(ans2);
+//            throw new RuntimeException("Oops");
+//        }
+
+    }
 }
